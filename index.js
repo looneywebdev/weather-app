8e2b1f2b3515aa8ce0d76dd7ac66956644cb4024
Heather Looney <looneydevelopment@gmail.com>
2018-05-14 19:09:40 -0700

    initial commit

---
+++ index.js
@@ -0,0 +1,157 @@
+// var latitude = 0
+// var longitude = 0
+
+function success(position) {
+    console.log('successful');
+    var lat = position.coords.latitude;
+    var long = position.coords.longitude;
+    callApi(lat, long, displayCurrent);
+    callSunsetApi(lat, long, initialSunset);
+    buttonClicks(lat,long);
+}
+
+function buttonClicks(lat, long) {
+    handleForecast(lat, long);
+    handleSunrise(lat, long);
+    handleAlmanac(lat, long);
+    handleCurrent(lat, long);
+}
+
+function handleForecast(lat, long) {
+    console.log("Listening for click on Forecast");
+    $('.forecast').on('click', function (ev){   
+        ev.preventDefault();
+        callForecastApi(lat,long, displayForecast);
+    });
+}
+
+function handleSunrise(lat, long) {
+    console.log("Listening for click on Sunrise");
+    $('.sunrise').on('click', function(ev) {
+        ev.preventDefault();
+        callSunsetApi(lat, long, displaySunrise);
+    });
+}
+
+function handleAlmanac(lat, long) {
+    console.log("Listening for click on Almanac");
+    $('.almanac').on('click', function(ev){
+        ev.preventDefault();
+        callAlmanacApi(lat, long, displayAlmanac);
+    });
+}
+
+function handleCurrent(lat, long) {
+    console.log("Listening for click on Current");
+    $('.current').on('click', function(ev){
+        ev.preventDefault();
+        callCurrentApi(lat, long, displayCurrent);
+    });
+}
+
+
+function error(position) {
+      console.log('refresh browser and allow it to see your location');
+}
+
+  //function for current conditions given lat, long
+
+function callApi(latitude, longitude, callback) {
+    const settings =  {
+        url: `https://api.wunderground.com/api/5f1b6d8103e5bf4c/conditions/q/${latitude},${longitude}.json`,
+        dataType: "jsonp",
+        success: callback
+    };
+    $.ajax(settings);
+}
+
+function initialSunset(results) {
+    const sunsetTime = `${results.sun_phase.sunset.hour}:${results.sun_phase.sunset.minute}`;
+    
+    const nowTime = moment(Date.now()).format('HH:mm');
+    //
+    const isDay = moment(sunsetTime, 'HH:mm').isAfter(moment(nowTime, 'HH:mm'));
+    if (isDay) {
+        $('body').addClass('day');
+    } else {
+        $('body').addClass('night');
+    }
+}
+
+function displayCurrent(result) {
+    $('.city').html(`<h2 class="city-name">${result.current_observation.display_location.full}</h2>`);
+    $('.city').append(`<img src="${result.current_observation.icon_url}">`);
+    $('.city').append(`<p>Current Temperature: ${result.current_observation.temperature_string}</p>`);
+    $('.city').append(`<p>The relative humidity is: ${result.current_observation.relative_humidity}</p>`);
+    $('.city').append(`<p>Wind: ${result.current_observation.wind_string}</p>`);
+    $('.buttons').toggleClass("hidden");
+    $('.js-search-results').html(`<p>Click a button above to get awesome weather data!</p>`);
+}
+//function that renders result content structure
+
+function renderResult(result) {
+    return `<div class="forecast-data">
+        <h3>${result.title}</h3>
+        <img src="${result.icon_url}">
+        <p>${result.fcttext}</p>
+      </div>`;
+}
+  //function that displays data in html to the end user
+
+function displayForecast(result) {
+    const results = result.forecast.txt_forecast.forecastday.map(renderResult);
+    $('.js-search-results').html(results);
+}
+
+function displayAlmanac(result) {
+    $('.js-search-results').html(`<p>Record High: ${result.almanac.temp_high.record.F} degrees F in ${result.almanac.temp_high.recordyear}</p>`);
+    $('.js-search-results').append(`<p>Record Low: ${result.almanac.temp_low.record.F} degrees F in ${result.almanac.temp_low.recordyear}</p>`);
+}
+
+function displaySunrise(result) {
+    $('.js-search-results').html(`<p>Sunrise Time: ${result.sun_phase.sunrise.hour}:${result.sun_phase.sunrise.minute}</p>`);
+    $('.js-search-results').append(`<p>Sunset Time: ${result.sun_phase.sunset.hour}:${result.sun_phase.sunset.minute}</p>`);
+}
+
+function callForecastApi(latitude, longitude, callback) {
+    const settings =  {
+        url: `https://api.wunderground.com/api/5f1b6d8103e5bf4c/forecast/q/${latitude},${longitude}.json`,
+        dataType: "jsonp",
+        success: callback
+        };
+        $.ajax(settings);
+}
+
+function callSunsetApi(latitude, longitude, callback) {
+    const settings = {
+        url: `https://api.wunderground.com/api/5f1b6d8103e5bf4c/astronomy/q/${latitude},${longitude}.json`,
+        dataType: "jsonp",
+        success: callback
+    };
+    $.ajax(settings);
+}
+
+function callAlmanacApi(latitude, longitude, callback) {
+    const settings = {
+        url: `https://api.wunderground.com/api/5f1b6d8103e5bf4c/almanac/q/${latitude},${longitude}.json`,
+        dataType: "jsonp",
+        success: callback
+    };
+    $.ajax(settings);
+}
+
+function callCurrentApi(latitude, longitude, callback) {
+    const settings =  {
+        url: `https://api.wunderground.com/api/5f1b6d8103e5bf4c/conditions/q/${latitude},${longitude}.json`,
+        dataType: "jsonp",
+        success: callback
+    };
+    $.ajax(settings);
+}
+
+function getLocation() {
+  //geolocation api call
+  navigator.geolocation.getCurrentPosition(success, error);
+}
+
+getLocation();
